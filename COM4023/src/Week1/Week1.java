4ac1ab9a1248cc5f900cb6cc94843cfa7294eeb4
Andrew <Andrew@DESKTOP-4VDCK19>
2024-09-27 12:06:03 +0100

    exercise 2 completed

---
+++ COM4023/src/Week1/Week1.java
@@ -0,0 +1,24 @@
+package Week1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Andrew Stephenson
+ */
+public class Week1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Exercise 1
+        System.out.println("Hello World");
+        
+        // Exercise 2
+       Scanner reader = new Scanner(System.in);
+       String name=reader.nextLine();
+       System.out.println("Your name is "+ name);
+    }
+    
+}
