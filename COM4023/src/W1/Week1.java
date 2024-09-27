09dffea8181c5e377fff7245b89cbb075e537978
Andrew <Andrew@DESKTOP-4VDCK19>
2024-09-27 12:04:38 +0100

    exercise 2 completed

--- COM4023/src/W1/Week1.java
+++ COM4023/src/W1/Week1.java
@@ -1,12 +1,15 @@
+package W1;
+
+import java.util.Scanner;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package Week1;
 
 /**
  *
- * @author Andrew
+ * @author Andrew Stephenson
  */
 public class Week1 {
 
@@ -17,7 +20,10 @@ public class Week1 {
         // Exercise 1
         System.out.println("Hello World");
         
-        
+        // Exercise 2
+       Scanner reader = new Scanner(System.in);
+       String name=reader.nextLine();
+       System.out.println("Your name is "+ name);
     }
     
 }
